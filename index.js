9f18eae652c722f27021bb40744f8c6c497c0fa5
hanaessam <hanaessamsayed@gmail.com>
2024-02-07 17:26:37 +0200

    VSCode Controller : get snippet of code from extension

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+
+const bodyParser = require('body-parser');
+const VSCodeController = require('./vscode-gateway/VSCodeController');
+
 const app = express();
 const port = 8888; 
+const vscodeController = new VSCodeController();
+// Middleware to parse JSON request body
+app.use(bodyParser.json());
 
-// Define a route
+
+// Define init route
 app.get('/', (req, res) => {
     res.send('Hello World! This is your first web service.');
 });
+
+// Create an instance of the controller class
+const vscodeRouter = express.Router();
+
+// Bind the controller method to the controller instance
+const getHighlightedCodeHandler = vscodeController.getHighlightedCode.bind(vscodeController);
+
+// Define the endpoint routes for vscode controller
+vscodeRouter.post('/highlight', getHighlightedCodeHandler);
+
+// Mount the router on the '/vscode' path
+app.use('/vscode', vscodeRouter);
 
 // Start the server
 app.listen(port, () => {
---
+++ vscode-gateway/VSCodeController.js
@@ -0,0 +1,13 @@
+class VSCodeController {
+    constructor() {
+       this.highlightedCode = "";
+    }
+
+    getHighlightedCode(req, res) {
+        this.highlightedCode = req.body.highlightedCode;
+        console.log('Highlighted text received:', this.highlightedCode);
+        res.status(200).send('Received highlighted text successfully.');
+    }
+}
+
+module.exports = VSCodeController;
