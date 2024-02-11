cba7c9504b7fd9fe8ff5a3f3de4d5825162a48b3
hanaessam <hanaessamsayed@gmail.com>
2024-02-11 13:14:53 +0200

    github authentication logic and endpoints

--- index.js
+++ index.js
@@ -1,56 +1,69 @@
-const express = require('express');
-
-const bodyParser = require('body-parser');
-const VSCodeController = require('./vscode-gateway/VSCodeController');
-const openAiController = require('./openAI-gateway/OpenAIController');
-const ChatController = require('./chat-module/ChatController');
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require('express-session');
+const VSCodeController = require("./vscode-gateway/VSCodeController");
+const openAiController = require("./openAI-gateway/OpenAIController");
+const ChatController = require("./chat-module/ChatController");
+const GitHubAuth = require("./github-oauth/GitHubOAuth");
+// Create an instance of the express server
 const app = express();
 const port = 8888;
+
+// Initialize express session
+app.use(session({
+    secret: 'any string',
+    resave: false,
+    saveUninitialized: true,
+  }));
+
+// Github OAuth setup
+const gitHubAuth = new GitHubAuth(app);
+gitHubAuth.initialize();
+gitHubAuth.setupGitHubStrategy();
+gitHubAuth.setupGitHubRoutes();
+
 const vscodeController = new VSCodeController();
 const openaiController = new openAiController();
 const chatController = new ChatController();
 // Middleware to parse JSON request body
 app.use(bodyParser.json());
 
-
 // Define init route
-app.get('/', (req, res) => {
-    res.send('Hello World! This is your first web service.');
+app.get("/", (req, res) => {
+  res.send("Hello World! This is your first web service.");
 });
 
 // Create an instance of the controller class
 const vscodeRouter = express.Router();
 
 // Bind the controller method to the controller instance
-const getHighlightedCodeHandler = vscodeController.getHighlightedCode.bind(vscodeController);
+const getHighlightedCodeHandler =
+  vscodeController.getHighlightedCode.bind(vscodeController);
 
 // Define the endpoint routes for vscode controller
-vscodeRouter.post('/highlight', getHighlightedCodeHandler);
+vscodeRouter.post("/highlight", getHighlightedCodeHandler);
 
 // Create an instance of the controller class
 const openaiRouter = express.Router();
 
 // Bind the openai controller method to the controller instance
-const processPromptHandler = openaiController.processPrompt.bind(openaiController);
+const processPromptHandler =
+  openaiController.processPrompt.bind(openaiController);
 
 // Define the endpoint routes for api controller
-openaiRouter.post('/processPrompt', processPromptHandler);
+openaiRouter.post("/processPrompt", processPromptHandler);
 
 // Bind the openai controller method to the controller instance
 const prompthandler = chatController.processInput.bind(chatController);
 
 // Define the endpoint routes for api controller
-openaiRouter.post('/prompt', prompthandler);
+openaiRouter.post("/prompt", prompthandler);
 
 // Mount the router on the '/vscode' path
-app.use('/vscode', vscodeRouter);
-app.use('/openai', openaiRouter);
-
+app.use("/vscode", vscodeRouter);
+app.use("/openai", openaiRouter);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-
+  console.log(`Server is running on http://localhost:${port}`);
 });
-
-
