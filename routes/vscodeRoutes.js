a7e0d845a4fbd1caf29cf85c615a04d7a1c9a174
hanaessam <hanaessamsayed@gmail.com>
2024-06-14 21:11:13 +0300

    generate project endpoint

--- routes/vscodeRoutes.js
+++ routes/vscodeRoutes.js
@@ -4,4 +4,10 @@ const vscodeRouter = express.Router();
 const vscodeController = new VSCodeController();
 const getHighlightedCodeHandler = vscodeController.getHighlightedCode.bind(vscodeController);
 vscodeRouter.post("/highlight", getHighlightedCodeHandler);
+vscodeRouter.post('/generate-project', async (req, res) => {
+    const { projectDescription } = req.body;
+    // Call OpenAI API or your logic to generate project architecture
+    const projectStructure = await vscodeController.generateProjectStructure(projectDescription);
+    res.json(projectStructure);
+  });
 module.exports = vscodeRouter;
--- vscode-gateway/VSCodeController.js
+++ vscode-gateway/VSCodeController.js
@@ -1,15 +1,26 @@
 class VSCodeController {
-    constructor() {
-        this.highlightedCode = "";
-    }
+  constructor() {
+    this.highlightedCode = "";
+  }
 
-    getHighlightedCode(req, res) {
-        this.highlightedCode = req.body.highlightedCode;
-        console.log('Highlighted text received:', this.highlightedCode);
-        res.status(200).send(this.highlightedCode);
-    }
+  getHighlightedCode(req, res) {
+    this.highlightedCode = req.body.highlightedCode;
+    console.log("Highlighted text received:", this.highlightedCode);
+    res.status(200).send(this.highlightedCode);
+  }
 
+  generateProjectStructure(description) {
+    // Logic to generate project structure
+    return {
+      folders: ["src", "test", "docs"],
+      files: {
+        "src/index.js": 'console.log("Hello, world!");',
+        "README.md": "# Project\nThis is a generated project.",
+      },
+    };
+  }
 
+  
 }
 
 module.exports = VSCodeController;
